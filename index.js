c6a556442a26d044f25d309dd7dd475f7c639c0f
boyboi86 <boyboi86@gmail.com>
2016-08-31 16:12:37 +0800

    remove depency

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-import ReactBootstrap, { Accordion } from 'react-bootstrap';
 
-import Hello from './hello.jsx';
-import World from './world.jsx';
+import Accordion from './Components/Accordion.jsx';
+import Modal from './Components/Modal.jsx';
 
 class App extends Component {
 
 	render() {
 		return (
 			<div>
-				<Hello />
-				<World />
+				<Accordion />
+				<Modal />
 			</div>
 		)
 	}
